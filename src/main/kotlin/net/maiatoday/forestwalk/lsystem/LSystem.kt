786231e8ff858bf2f6f3182e396a39bcb7de1a01
Maia Grotepass <maiatoday@gmail.com>
2024-03-10 19:20:44 +0200

    Fresh new tortoise

--- src/main/kotlin/net/maiatoday/forestwalk/lsystem/LSystem.kt
+++ src/main/kotlin/net/maiatoday/forestwalk/lsystem/LSystem.kt
@@ -2,61 +2,46 @@ package net.maiatoday.forestwalk.lsystem
 
 import java.awt.Color
 import java.awt.Graphics2D
-import java.awt.RenderingHints
-import java.awt.image.BufferedImage
+import kotlin.math.cos
+import kotlin.math.sin
 
-data class Segment(var x: Double, var y: Double, var angle: Double)
+data class State(val x: Int, val y: Int, val length: Int, val angle: Double = 0.0, val colorIndex:Int = 0)
 
-fun tortoiseDraw(lsystem: String, iterations: Int): BufferedImage {
-    val width = 1000
-    val height = 1000
-    val angleChange = Math.PI / 3  // 60 degrees
-    val initialLength = height / (2.0 * iterations)
-    val lengthDecreaseFactor = 0.6
-    val stack = mutableListOf<Segment>()
-    var segment = Segment(width / 2.0, 0.9 * height, -Math.PI / 2)
-    var length = initialLength
-    val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-    val g2d = bufferedImage.createGraphics()
-    with(g2d) {
-        setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-        color = Color.BLACK
-        fillRect(0, 0, width, height)
-        color = Color.GREEN
-        lsystem.forEach { c -> segment = drawSegment(c, segment, stack, angleChange, lengthDecreaseFactor, length) }
-        dispose()
-    }
-    return bufferedImage
-}
+fun Graphics2D.goTortoise(
+    penColors: List<Color> = listOf(Color.GREEN),
+    angleChange:Double = Math.PI/3,
+    length:Int = 20,
+    startX: Int,
+    startY: Int,
+    plant: Plant
+) {
+    val stack = ArrayDeque<State>()
+    var state = State(startX, startY , length, -Math.PI / 2)
+    plant.bluePrint.forEach { c ->
+        state = when (c) {
+            'F', 'f' -> {
+                val newX = state.x + (state.length * cos(state.angle)).toInt()
+                val newY = state.y + (state.length * sin(state.angle)).toInt()
+                if (c == 'F') {
+                    color = penColors[state.colorIndex]
+                    drawLine(state.x, state.y, newX, newY)
+                }
+                state.copy(x = newX, y = newY)
+            }
 
-private fun Graphics2D.drawSegment(
-    c: Char, segment: Segment, stack: MutableList<Segment>, angleChange: Double,
-    lengthDecreaseFactor: Double, length: Double
-): Segment {
-    var newLength = length
-    var newSegment = segment
-    when (c) {
-        'F' -> {
-            val newX = segment.x + length * Math.cos(segment.angle)
-            val newY = segment.y + length * Math.sin(segment.angle)
-            drawLine(segment.x.toInt(), segment.y.toInt(), newX.toInt(), newY.toInt())
-            newSegment = Segment(newX, newY, segment.angle)
-        }
+            '+' -> state.copy(angle = state.angle - angleChange)
+            '-' -> state.copy(angle = state.angle + angleChange)
+            '`' -> state.copy(colorIndex = (state.colorIndex+1)%penColors.size)
+            '[' -> {
+                stack.addLast(state)
+                state
+            }
 
-        '+' -> newSegment = Segment(segment.x, segment.y, segment.angle - angleChange)
-        '-' -> newSegment = Segment(segment.x, segment.y, segment.angle + angleChange)
-        '[' -> {
-            stack.add(Segment(segment.x, segment.y, segment.angle))
-            newLength *= lengthDecreaseFactor
-        }
+            ']' -> stack.removeLast()
 
-        ']' -> {
-            newSegment = stack.removeAt(stack.size - 1)
-            newLength /= lengthDecreaseFactor
-        }
+            else ->
+                state
 
-        else -> {
         }
     }
-    return newSegment
 }
