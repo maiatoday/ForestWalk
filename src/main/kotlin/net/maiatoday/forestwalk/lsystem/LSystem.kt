723e45013975047615bb5c82da6d11a1ea26bad7
Maia Grotepass <maiatoday@gmail.com>
2024-03-30 20:41:23 +0100

    add the squirrel

--- src/main/kotlin/net/maiatoday/forestwalk/lsystem/LSystem.kt
+++ src/main/kotlin/net/maiatoday/forestwalk/lsystem/LSystem.kt
@@ -5,6 +5,7 @@ import java.awt.Color
 import java.awt.Graphics2D
 import kotlin.math.cos
 import kotlin.math.sin
+import kotlin.random.Random
 
 typealias Degree = Double
 typealias Radian = Double
@@ -72,10 +73,50 @@ fun Graphics2D.goTortoise(
 fun Graphics2D.goSquirrel(
     penColors: List<Color> = listOf(Color.GREEN),
     angleChangeDeg:Degree = 20.0,
+    startWidth:Float = 10.0f,
+    widthChange:Float = 0.95f,
     length:Int = 20,
     startX: Int,
     startY: Int,
     bluePrint: String
 ) {
+    val angleChange = angleChangeDeg.toRadian()
+    val stack = ArrayDeque<State>()
+    var state = State(
+        x = startX,
+        y = startY,
+        length = length,
+        angle = -Math.PI / 2, // upright trees
+        strokeWidth = startWidth
+    )
+    stroke = BasicStroke(state.strokeWidth)
+    bluePrint.forEach { c ->
+        val jitter = Random.nextDouble(-5.0.toRadian(), 5.0.toRadian())
+        state = when (c) {
+            'F', 'f' -> {
+                val newX = state.x + (state.length * cos(state.angle)).toInt()
+                val newY = state.y + (state.length * sin(state.angle)).toInt()
+                if (c == 'F') {
+                    color = penColors[state.colorIndex]
+                    drawLine(state.x, state.y, newX, newY)
+                }
+                state.copy(x = newX, y = newY, strokeWidth = state.strokeWidth*widthChange)
+            }
 
+            '+' -> state.copy(angle = state.angle - (angleChange +jitter))
+            '-' -> state.copy(angle = state.angle + (angleChange + jitter))
+            '`' -> state.copy(colorIndex = (state.colorIndex+1)%penColors.size)
+            '[' -> {
+                stack.addLast(state)
+                state
+            }
+
+            ']' -> stack.removeLast()
+
+            else ->
+                state
+
+        }
+        stroke = BasicStroke(state.strokeWidth)
+    }
 }
