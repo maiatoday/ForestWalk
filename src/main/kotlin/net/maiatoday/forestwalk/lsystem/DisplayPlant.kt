08982de89a16736d6ac17bd3214430625861d97b
Maia Grotepass <maiatoday@gmail.com>
2024-04-15 22:39:16 +0200

    better dataframes with fore/middle/back ground

--- src/main/kotlin/net/maiatoday/forestwalk/lsystem/DisplayPlant.kt
+++ src/main/kotlin/net/maiatoday/forestwalk/lsystem/DisplayPlant.kt
@@ -1,5 +1,9 @@
 package net.maiatoday.forestwalk.lsystem
+
+import net.maiatoday.forestwalk.tools.shrubShades
+import net.maiatoday.forestwalk.tools.treeShades
 import java.awt.Color
+import kotlin.random.Random
 
 enum class Ground { Fore, Middle, Back }
 data class Seedling(
@@ -12,3 +16,13 @@ data class Seedling(
     val startY: Int,
     val bluePrint: String
 )
+
+fun colorSeedling(isTree: Boolean, ground: Ground): List<Color> =
+    if (isTree) treeShades.shuffled().take(1)
+    else shrubShades.shuffled().take(1)
+
+fun xSeedling(isTree: Boolean, ground: Ground, width: Int): Int = Random.nextInt(0, width)
+fun ySeedling(isTree: Boolean, ground: Ground, height: Int): Int = Random.nextInt(height - 150, height)
+fun lengthSeedling(isTree: Boolean, ground: Ground): Int = if (isTree) 5 else 6
+fun startStrokeSeedling(isTree: Boolean, ground: Ground): Float = if (isTree) 15.0f else 8.0f
+fun strokeChangeSeedling(isTree: Boolean, ground: Ground): Float = if (isTree) 0.98f else 0.9f
