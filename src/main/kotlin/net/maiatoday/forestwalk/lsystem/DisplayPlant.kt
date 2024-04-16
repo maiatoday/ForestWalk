bafa162466e896ec35805b82a497bfb8ef605dee
Maia Grotepass <maiatoday@gmail.com>
2024-04-16 20:16:04 +0200

    fore mod back ground

--- src/main/kotlin/net/maiatoday/forestwalk/lsystem/DisplayPlant.kt
+++ src/main/kotlin/net/maiatoday/forestwalk/lsystem/DisplayPlant.kt
@@ -18,11 +18,54 @@ data class Seedling(
 )
 
 fun colorSeedling(isTree: Boolean, ground: Ground): List<Color> =
-    if (isTree) treeShades.shuffled().take(1)
-    else shrubShades.shuffled().take(1)
+    if (isTree) {
+        when (ground) {
+            Ground.Fore -> treeShades.shuffled().take(1)
+            Ground.Middle -> listOf(Color.gray)
+            Ground.Back -> listOf(Color.lightGray)
+        }
+    } else {
+        shrubShades.shuffled().take(1)
+    }
 
 fun xSeedling(isTree: Boolean, ground: Ground, width: Int): Int = Random.nextInt(0, width)
-fun ySeedling(isTree: Boolean, ground: Ground, height: Int): Int = Random.nextInt(height - 150, height)
-fun lengthSeedling(isTree: Boolean, ground: Ground): Int = if (isTree) 5 else 6
-fun startStrokeSeedling(isTree: Boolean, ground: Ground): Float = if (isTree) 15.0f else 8.0f
-fun strokeChangeSeedling(isTree: Boolean, ground: Ground): Float = if (isTree) 0.98f else 0.9f
+
+fun ySeedling(isTree: Boolean, ground: Ground, height: Int): Int = if (isTree) {
+    when (ground) {
+        Ground.Fore -> Random.nextInt(height - 50, height)
+        Ground.Middle -> Random.nextInt(height - 100, height - 50)
+        Ground.Back -> Random.nextInt(height - 200, height - 150)
+    }
+} else {
+    height
+}
+
+fun lengthSeedling(isTree: Boolean, ground: Ground): Int = if (isTree) {
+    when (ground) {
+        Ground.Fore -> 5
+        Ground.Middle -> 4
+        Ground.Back -> 4
+    }
+} else {
+    6
+}
+
+fun startStrokeSeedling(isTree: Boolean, ground: Ground): Float = if (isTree) {
+    when (ground) {
+        Ground.Fore -> 15.0f
+        Ground.Middle -> 10.0f
+        Ground.Back -> 9.0f
+    }
+} else {
+    8.0f
+}
+
+fun strokeChangeSeedling(isTree: Boolean, ground: Ground): Float = if (isTree) {
+    when (ground) {
+        Ground.Fore -> 0.98f
+        Ground.Middle -> 0.98f
+        Ground.Back -> 0.98f
+    }
+} else {
+    0.9f
+}
